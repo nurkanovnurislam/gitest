e6967bdb92ff445eb7edd83cbbac546a4822df6e
nurislamnurkanov <nurislamnurkanov@gmail.com>
2022-10-24 16:11:16 +0600

    2test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("gitest");
+        System.out.println("bul git test keregi jok");
+        System.out.println("dfdfdg");
     }
 }
